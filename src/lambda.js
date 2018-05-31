c7d774563f0349da5a148055e338eb0f2741c3a5
safoura.boyot@gmail.com <safoura.boyot@gmail.com>
2018-05-31 14:30:17 +0100

    add dynamo db config and add post and get request

--- src/lambda.js
+++ src/lambda.js
@@ -1,4 +1,22 @@
-exports.handler = function (event, context) {
-	console.log(event);
-	context.succeed('hello ' + event.name);
-};
+const ApiBuilder = require('claudia-api-builder'),
+    AWS = require('aws-sdk');
+var api = new ApiBuilder(),
+    dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+api.post('/reports', function (request) { // SAVE your report
+    var params = {
+        TableName: 'reports',
+        Item: {
+            reportId: request.body.reportId,
+            input_fields: request.body.input_fields // your report name
+        }
+    }
+    return dynamoDb.put(params).promise(); // returns dynamo result
+}, { success: 201 }); // returns HTTP status 201 - Created if successful
+
+api.get('/reports', function (request) { // GET all users
+    return dynamoDb.scan({ TableName: 'reports' }).promise()
+        .then(response => response.Items)
+});
+
+module.exports = api;
