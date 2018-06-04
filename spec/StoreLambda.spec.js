e1c7008945007de5c6774f0beb6d3ddea5ac4d85
safoura.boyot@gmail.com <safoura.boyot@gmail.com>
2018-06-04 13:46:26 +0100

    For testing mergetool from terminal

--- spec/StoreLambda.spec.js
+++ spec/StoreLambda.spec.js
@@ -1,31 +1,31 @@
 const request = require('request');
 // export for others scripts to use
 
-describe("Store Lambda", function() {
+describe("Store Lambda", function () {
 
-	it("calls DynamoDB with the data provided by the Ajax request", function(done) {
-		const pstUrl = "https://y77j5js7md.execute-api.us-east-1.amazonaws.com/dev/reports"
-		const reportId ="123345"
-		const postData = {
-			reportId: reportId,
-			input_fields: "input-fields",
-		}
+    it("calls DynamoDB with the data provided by the Ajax request", function (done) {
+        const pstUrl = "https://y77j5js7md.execute-api.us-east-1.amazonaws.com/dev/reports"
+        const reportId = "123345"
+        const postData = {
+            reportId: reportId,
+            input_fields: "input-fields",
+        }
 
-		const options = {
-  			method: 'post',
-  			body: postData,
-  			json: true,
-  			url: pstUrl
-		}
+        const options = {
+            method: 'post',
+            body: postData,
+            json: true,
+            url: pstUrl
+        }
 
-		request(options, function(err, res) {
-			done();
-		})
+        request(options, function (err, res) {
+            done();
+        })
 
-		const getUrl ="https://y77j5js7md.execute-api.us-east-1.amazonaws.com/dev/reports/"+reportId
-		request.get(getUrl, function(err, res, body) {
-			expect(body).toBe([postData.toString()]);
-			done();
-		})
-	})
+        const getUrl = "https://y77j5js7md.execute-api.us-east-1.amazonaws.com/dev/reports/" + reportId
+        request.get(getUrl, function (err, res, body) {
+            expect(body).toBe(postData.toString());
+            done();
+        })
+    })
 })
