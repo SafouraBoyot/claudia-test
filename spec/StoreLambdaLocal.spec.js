cfdf542d2c7bccece0287da251c4b7558b4fb1a4
safoura.boyot@gmail.com <safoura.boyot@gmail.com>
2018-06-05 11:38:36 +0100

    make local test pass by assigning local document clinet for dynamoDb

--- spec/StoreLambdaLocal.spec.js
+++ spec/StoreLambdaLocal.spec.js
@@ -6,28 +6,35 @@ const localDynamo = require('local-dynamo')
 
 localDynamo.launch(null, 4567);
 
+var AWS = require("aws-sdk");
 
+AWS.config.update({
+    region: "us-west-2",
+    endpoint: "http://localhost:8000"
+});
+var documentClient = new AWS.DynamoDB.DocumentClient();
 
 describe('Store Lambda', () => {
-	var lambdaContextSpy;
-	assignDatabase(localDynamo);
 
-	beforeEach(() => {
-		lambdaContextSpy = jasmine.createSpyObj('lambdaContext', ['done']);
-	});
+    var lambdaContextSpy;
+    assignDatabase(documentClient);
 
-	it('it stores and retrieves a report', () => {
-		underTest.proxyRouter({
-			requestContext: {
-        		resourcePath: '/reports',
-        		httpMethod: 'POST'
-	      	},
-	      	body: {
-				reportId: "123456",
-				input_fields: "input-fields"
-	      	}
-		}, lambdaContextSpy).then((err, res, body) => {
-			expect(body.status).toEqual(201)
-		})
-	})
+    beforeEach(() => {
+        lambdaContextSpy = jasmine.createSpyObj('lambdaContext', ['done']);
+    });
+
+    it('it stores and retrieves a report', () => {
+        underTest.proxyRouter({
+            requestContext: {
+                resourcePath: '/reports',
+                httpMethod: 'POST'
+            },
+            body: {
+                reportId: 123456,
+                input_fields: "input-fields"
+            }
+        }, lambdaContextSpy).then((err, res, body) => {
+            expect(res.statusCode).toEqual(201)
+        })
+    })
 })
