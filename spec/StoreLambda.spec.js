e2ef52e4fba9e042593c678a081ca9977906f6f7
safoura.boyot@gmail.com <safoura.boyot@gmail.com>
2018-06-05 10:47:06 +0100

    try to add local-dynamo

--- spec/StoreLambda.spec.js
+++ spec/StoreLambda.spec.js
@@ -2,10 +2,10 @@ const request = require('request');
 // export for others scripts to use
 
 describe("Store Lambda", function () {
-    
+
 	it("calls DynamoDB with the data provided by the Ajax request", function(done) {
 		const pstUrl = "https://y77j5js7md.execute-api.us-east-1.amazonaws.com/dev/reports"
-		const reportId ="123345"
+		const reportId ="3"
 		const postData = {
 			reportId: reportId,
 			input_fields: "input-fields",
