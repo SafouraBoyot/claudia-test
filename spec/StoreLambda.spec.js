7c2d82cb1f3df4f42dfd944d7edb84336c68f62c
safoura.boyot@gmail.com <safoura.boyot@gmail.com>
2018-06-06 14:51:25 +0100

    update calls DynamoDB with the data provided by the Ajax request test to make sure get request calls after post

--- spec/StoreLambda.spec.js
+++ spec/StoreLambda.spec.js
@@ -1,7 +1,7 @@
 const request = require('request');
+const requestPromise = require('request-promise');
 
 describe("Store Lambda", function () {
-
     function uuidv4() {
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
             var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
@@ -9,7 +9,7 @@ describe("Store Lambda", function () {
         });
     }
 
-    it("calls Lambda API with the data provided by the Ajax request", function (done) {
+    it("calls DynamoDB with the data provided by the Ajax request", function (done) {
         const postUrl = "https://y77j5js7md.execute-api.us-east-1.amazonaws.com/dev/reports"
 
         const reportId = uuidv4()
@@ -25,24 +25,17 @@ describe("Store Lambda", function () {
             url: postUrl
         }
 
-        request(options, function (err, res) {
-            done();
-        })
-
         const getUrl = `https://y77j5js7md.execute-api.us-east-1.amazonaws.com/dev/reports/${reportId}`;
 
-        request.get(getUrl, function (err, res, body) {
-            expect(body).toBe(JSON.stringify(postData));
-            done();
+        request(options, function (err, res) {
+            expect(res.statusCode).toEqual(201)
+
+            request.get(getUrl, function (err, res, body) {
+                expect(res.statusCode).toEqual(200)
+                expect(body).toBe(JSON.stringify(postData));
+                done();
+            })
         })
-    })
 
-    it("calls Lambda API to getting all the reports", function () {
-
-        const getUrl = "https://y77j5js7md.execute-api.us-east-1.amazonaws.com/dev/reports";
-
-        request.get(getUrl, function (err, res, body) {
-            expect(res.statusCode).toEqual(200);
-        })
     })
 })
