9c510790325f3d527526c0b579baa3cab4399f53
safoura.boyot@gmail.com <safoura.boyot@gmail.com>
2018-06-08 14:49:55 +0100

    add convertReport api to lambda.js

--- src/lambda.js
+++ src/lambda.js
@@ -1,6 +1,8 @@
 import API from 'claudia-api-builder';
 import AWS from 'aws-sdk';
 const tools = require ('../src/tools.js');
+const converter = require('../src/html-to-pdf-converter');
+
 const api = new API();
 var dynamoDb = new AWS.DynamoDB.DocumentClient();
 
@@ -43,15 +45,16 @@ api.get('/reports/{id}', (request) => {
 });
 
 api.post('/convertReport', (request) => {
+    const reportId =request.body.reportId;
     var params = {
         TableName: 'reports',
         Item: {
-            reportId: request.body.reportId,
+            reportId: reportId,
             input_fields: request.body.input_fields,
 
         }
     }
-
+    // converter.htmlToPdf(html,reportId); Todo : create html file based on the input_fields
     return dynamoDb.put(params).promise();
 }, {success: 201});
 module.exports = {api, assignDatabase};
