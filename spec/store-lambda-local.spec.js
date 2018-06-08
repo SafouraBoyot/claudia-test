f98ff33bc4f35252d415e90c631e3a6d09d8ffec
safoura.boyot@gmail.com <safoura.boyot@gmail.com>
2018-06-08 16:46:00 +0100

    for pull

--- spec/store-lambda-local.spec.js
+++ spec/store-lambda-local.spec.js
@@ -1,59 +1,59 @@
-const tools = require ('../src/tools.js');
-
-const lambda = require('../dist/lambda.js');
-const rp = require('request-promise');
-const underTest = lambda.api;
-var assignDatabase = lambda.assignDatabase;
-
-const localDynamo = require('local-dynamo')
-
-localDynamo.launch(null, 4567);
-
-var AWS = require("aws-sdk");
-
-AWS.config.update({
-    apiVersion: "2012-08-10",
-    region: "eu-west-1",
-    endpoint: "http://localhost:4567"
-});
-var documentClient = new AWS.DynamoDB.DocumentClient();
-
-describe('Store Lambda', function () {
-
-    var lambdaContextSpy;
-    assignDatabase(documentClient);
-
-    beforeEach(() => {
-        lambdaContextSpy = jasmine.createSpyObj('lambdaContext', ['done']);
-    });
-
-    it('it stores a report', () => {
-
-        const reportId =tools.uuidv4();
-        const postData = {
-            reportId: reportId,
-            input_fields: "input-fields",
-        }
-        underTest.proxyRouter({
-            requestContext: {
-                resourcePath: '/reports',
-                httpMethod: 'POST'
-            },
-            body:postData,
-            resolveWithFullResponse: true
-        }, lambdaContextSpy).then(() => {
-            expect(lambdaContextSpy.done).toHaveBeenCalledWith(null, jasmine.objectContaining({statusCode :201}));
-        })
-        underTest.proxyRouter({
-            requestContext: {
-                resourcePath: '/reports/{id}',
-                httpMethod: 'GET'
-            },
-            pathParameters: {
-                id: reportId
-            }
-        }, lambdaContextSpy).then(() => {
-            expect(lambdaContextSpy.done).toHaveBeenCalledWith(null, jasmine.objectContaining({body : postData}));
-        })
-    })
-})
+// const tools = require ('../src/tools.js');
+//
+// const lambda = require('../dist/lambda.js');
+// const rp = require('request-promise');
+// const underTest = lambda.api;
+// var assignDatabase = lambda.assignDatabase;
+//
+// const localDynamo = require('local-dynamo')
+//
+// localDynamo.launch(null, 4567);
+//
+// var AWS = require("aws-sdk");
+//
+// AWS.config.update({
+//     apiVersion: "2012-08-10",
+//     region: "eu-west-1",
+//     endpoint: "http://localhost:4567"
+// });
+// var documentClient = new AWS.DynamoDB.DocumentClient();
+//
+// describe('Store Lambda', function () {
+//
+//     var lambdaContextSpy;
+//     assignDatabase(documentClient);
+//
+//     beforeEach(() => {
+//         lambdaContextSpy = jasmine.createSpyObj('lambdaContext', ['done']);
+//     });
+//
+//     it('it stores a report', () => {
+//
+//         const reportId =tools.uuidv4();
+//         const postData = {
+//             reportId: reportId,
+//             input_fields: "input-fields",
+//         }
+//         underTest.proxyRouter({
+//             requestContext: {
+//                 resourcePath: '/reports',
+//                 httpMethod: 'POST'
+//             },
+//             body:postData,
+//             resolveWithFullResponse: true
+//         }, lambdaContextSpy).then(() => {
+//             expect(lambdaContextSpy.done).toHaveBeenCalledWith(null, jasmine.objectContaining({statusCode :201}));
+//         })
+//         underTest.proxyRouter({
+//             requestContext: {
+//                 resourcePath: '/reports/{id}',
+//                 httpMethod: 'GET'
+//             },
+//             pathParameters: {
+//                 id: reportId
+//             }
+//         }, lambdaContextSpy).then(() => {
+//             expect(lambdaContextSpy.done).toHaveBeenCalledWith(null, jasmine.objectContaining({body : postData}));
+//         })
+//     })
+// })
