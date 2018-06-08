e038aa7ebf3336136d1db0fd87687dd714372290
ggsbv <ggsbv@protonmail.com>
2018-06-08 14:46:15 +0100

    pushing in order to pull changes from Safoura

--- spec/createTable.spec.js
+++ spec/createTable.spec.js
@@ -1,58 +1,66 @@
-// var AWS = require("aws-sdk");
-//
-// AWS.config.update({
-//     region: "eu-west-1",
-//     endpoint: "http://localhost:4567"
-// });
-//
-// var docClient = new AWS.DynamoDB.DocumentClient();
-// ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
-// var tabaleParams = {
-//     AttributeDefinitions: [
-//         {
-//             AttributeName: 'reportId',
-//             AttributeType: 'S'
-//         }
-//     ],
-//     KeySchema: [
-//         {
-//             AttributeName: 'reportId',
-//             KeyType: 'HASH'
-//         }
-//     ],
-//     ProvisionedThroughput: {
-//         ReadCapacityUnits: 1,
-//         WriteCapacityUnits: 1
-//     },
-//     TableName: 'reports',
-//     StreamSpecification: {
-//         StreamEnabled: false
-//     }
-// };
-//
-// var params = {
-//     TableName: 'reports',
-//     Item: {
-//         reportId: "1234",
-//         input_fields: "input-fields",
-//     }
-// };
-//
-//
-// console.log("creating a new Table...");
-// ddb.createTable(tabaleParams, function (err, data) {
-//     if (err) {
-//         console.log("Error", err);
-//     } else {
-//         console.log("Success", data);
-//     }
-// });
-// console.log("Adding a new item...");
-//
-// docClient.put(params, function (err, data) {
-//     if (err) {
-//         console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-//     } else {
-//         console.log("Added item:", JSON.stringify(data, null, 2));
-//     }
-// });
+var AWS = require("aws-sdk");
+
+AWS.config.update({
+    region: "eu-west-1",
+    endpoint: "http://localhost:4567"
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
+var tableParams = {
+    AttributeDefinitions: [
+        {
+            AttributeName: 'reportId',
+            AttributeType: 'S'
+        },
+        {
+            AttributeName: 'input_fields',
+            AttributeType: 'S'
+        },
+        {
+            AttributeName: 'results',
+            AttributeType: 'S'
+        },
+    ],
+    KeySchema: [
+        {
+            AttributeName: 'reportId',
+            KeyType: 'HASH'
+        }
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 1,
+        WriteCapacityUnits: 1
+    },
+    TableName: 'reports',
+    StreamSpecification: {
+        StreamEnabled: false
+    }
+};
+
+var params = {
+    TableName: 'reports',
+    Item: {
+        reportId: "1234",
+        input_fields: "input-fields",
+    }
+};
+
+
+console.log("creating a new Table...");
+ddb.createTable(tableParams, function (err, data) {
+    if (err) {
+        console.log("Error", err);
+    } else {
+        console.log("Success", data);
+    }
+});
+console.log("Adding a new item...");
+
+docClient.put(params, function (err, data) {
+    if (err) {
+        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Added item:", JSON.stringify(data, null, 2));
+    }
+});
