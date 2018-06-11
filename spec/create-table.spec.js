978e3312990f23447123d75bd2a0871220a8bae9
ggsbv <ggsbv@protonmail.com>
2018-06-11 10:39:59 +0100

    exported createTable script

--- spec/create-table.spec.js
+++ spec/create-table.spec.js
@@ -1,59 +1,61 @@
-// var AWS = require("aws-sdk");
+var AWS = require("aws-sdk");
 
-// AWS.config.update({
-//     region: "eu-west-1",
-//     endpoint: "http://localhost:4567"
-// });
+AWS.config.update({
+    region: "eu-west-1",
+    endpoint: "http://localhost:4567"
+});
 
-// var docClient = new AWS.DynamoDB.DocumentClient();
-// ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
-// var tableParams = {
-//     AttributeDefinitions: [
-//         {
-//             AttributeName: 'reportId',
-//             AttributeType: 'S'
-//         },
-//     ],
-//     KeySchema: [
-//         {
-//             AttributeName: 'reportId',
-//             KeyType: 'HASH'
-//         },
-//     ],
-//     ProvisionedThroughput: {
-//         ReadCapacityUnits: 1,
-//         WriteCapacityUnits: 1
-//     },
-//     TableName: 'reports',
-//     StreamSpecification: {
-//         StreamEnabled: false
-//     }
-// };
+var docClient = new AWS.DynamoDB.DocumentClient();
+ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
+var tableParams = {
+    AttributeDefinitions: [
+        {
+            AttributeName: 'reportId',
+            AttributeType: 'S'
+        },
+    ],
+    KeySchema: [
+        {
+            AttributeName: 'reportId',
+            KeyType: 'HASH'
+        },
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 1,
+        WriteCapacityUnits: 1
+    },
+    TableName: 'reports',
+    StreamSpecification: {
+        StreamEnabled: false
+    }
+};
 
-// var params = {
-//     TableName: 'reports',
-//     Item: {
-//         reportId: "1234",
-//         input_fields: "input-fields",
-//         results: "results"
-//     }
-// };
+var params = {
+    TableName: 'reports',
+    Item: {
+        reportId: "1234",
+        input_fields: "input-fields",
+        results: "results"
+    }
+};
 
 
-// console.log("creating a new Table...");
-// ddb.createTable(tableParams, function (err, data) {
-//     if (err) {
-//         console.log("Error", err);
-//     } else {
-//         console.log("Success", data);
-//         console.log("Adding a new item...");
+console.log("creating a new Table...");
+const createTable = ddb.createTable(tableParams, function (err, data) {
+    if (err) {
+        console.log("Error", err);
+    } else {
+        console.log("Success", data);
+        console.log("Adding a new item...");
 
-//         docClient.put(params, function (err, data) {
-//             if (err) {
-//                 console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-//             } else {
-//                 console.log("Added item:", JSON.stringify(data, null, 2));
-//             }
-//         });
-//     }
-// });
+        docClient.put(params, function (err, data) {
+            if (err) {
+                console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+            } else {
+                console.log("Added item:", JSON.stringify(data, null, 2));
+            }
+        });
+    }
+});
+
+module.exports = { createTable, deleteTable }
